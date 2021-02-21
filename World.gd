e197ac8031e88de538c7d232e0ca59fb91fea3d6
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-02-20 19:02:57 -0600

    Woah
    Added Button
    added label
    added font
    added door (area2d)
    animated door
    door sends signal

--- World.gd
+++ World.gd
@@ -1,5 +1,20 @@
 extends Node2D
 func _ready():
-	pass
+	$Goal/AnimatedSprite.play('idle')
 func _on_DeathFloor_body_entered(body ):
 	$Player.position = $Position2D.position
+
+
+func _on_Goal_body_entered(body):
+	$Goal/AnimatedSprite.play('opening')
+	$Player.visible = false
+	$Player.position = $Position2D.position
+	$Label.visible = true
+	$Button.visible = true
+
+
+func _on_Button_pressed():
+	$Goal/AnimatedSprite.play('closing')
+	$Player.visible = true
+	$Label.visible = false
+	$Button.visible = false
